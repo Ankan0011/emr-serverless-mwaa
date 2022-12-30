bd54d64d15d1c5ede952cacf3f140675b2ce4ff9
Ankan0011 <ankan.ghosh@uzh.ch>
2022-12-31 00:31:17 +0100

    Policy Updates for MWAA

--- stack/mwaa.py
+++ stack/mwaa.py
@@ -84,3 +84,135 @@ class MwaaStack(Stack):
         # Register a couple outputs
         cdk.CfnOutput(self, "mwaa_bucket", value=bucket.bucket_name)
         cdk.CfnOutput(self, "mwaa_url", value=f"https://{airflow.attr_webserver_url}")
+
+        def emr_serverless_management_policy(self, emr_serverless_app_arn: str, emr_serverless_job_role_arn: str):
+            return iam.PolicyDocument(
+                statements=[
+                    iam.PolicyStatement(
+                        actions=[
+                            "emr-serverless:CreateApplication",
+                            "emr-serverless:GetApplication",
+                            "emr-serverless:StartApplication",
+                            "emr-serverless:StopApplication",
+                            "emr-serverless:DeleteApplication",
+                            "emr-serverless:StartJobRun",
+                            "emr-serverless:GetJobRun"
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources = ["*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "iam:PassRole",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=[emr_serverless_job_role_arn],
+                        conditions={
+                            "StringLike": {
+                                "iam:PassedToService": "emr-serverless.amazonaws.com"
+                            }
+                        },
+                    ),
+                ]
+            )
+        
+        def mwaa_policy_document(self, mwaa_env_name: str, mwaa_bucket_arn: str):
+            return iam.PolicyDocument(
+                statements=[
+                    iam.PolicyStatement(
+                        actions=["airflow:PublishMetrics"],
+                        effect=iam.Effect.ALLOW,
+                        resources=[
+                            f"arn:aws:airflow:{self.region}:{self.account}:environment/{mwaa_env_name}"
+                        ],
+                    ),
+                    iam.PolicyStatement(
+                        actions=["s3:ListAllMyBuckets"],
+                        effect=iam.Effect.DENY,
+                        resources=[f"{mwaa_bucket_arn}/*", f"{mwaa_bucket_arn}"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=["s3:*"],
+                        effect=iam.Effect.ALLOW,
+                        resources=[f"{mwaa_bucket_arn}/*", f"{mwaa_bucket_arn}"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "logs:CreateLogStream",
+                            "logs:CreateLogGroup",
+                            "logs:PutLogEvents",
+                            "logs:GetLogEvents",
+                            "logs:GetLogRecord",
+                            "logs:GetLogGroupFields",
+                            "logs:GetQueryResults",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=[
+                            f"arn:aws:logs:{self.region}:{self.account}:log-group:airflow-{mwaa_env_name}-*"
+                        ],
+                    ),
+                    iam.PolicyStatement(
+                        actions=["logs:DescribeLogGroups"],
+                        effect=iam.Effect.ALLOW,
+                        resources=["*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "sqs:ChangeMessageVisibility",
+                            "sqs:DeleteMessage",
+                            "sqs:GetQueueAttributes",
+                            "sqs:GetQueueUrl",
+                            "sqs:ReceiveMessage",
+                            "sqs:SendMessage",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=[f"arn:aws:sqs:{self.region}:*:airflow-celery-*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "kms:Decrypt",
+                            "kms:DescribeKey",
+                            "kms:GenerateDataKey*",
+                            "kms:Encrypt",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=["*"],
+                        conditions={
+                            "StringEquals": {
+                                "kms:ViaService": [
+                                    f"sqs.{self.region}.amazonaws.com",
+                                    f"s3.{self.region}.amazonaws.com",
+                                ]
+                            }
+                        },
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "emr-containers:StartJobRun",
+                            "emr-containers:DescribeJobRun",
+                            "emr-containers:CancelJobRun",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=["*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=[
+                            "elasticmapreduce:RunJobFlow",
+                            "elasticmapreduce:DescribeStep",
+                            "elasticmapreduce:DescribeCluster",
+                        ],
+                        effect=iam.Effect.ALLOW,
+                        resources=["*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=["iam:PassRole"],
+                        effect=iam.Effect.ALLOW,
+                        resources=[
+                            f"arn:aws:iam::{self.account}:role/EMR_DemoRole",
+                            f"arn:aws:iam::{self.account}:role/EMR_EC2_DemoRole",
+                            f"arn:aws:iam::{self.account}:role/EMR_EC2_DefaultRole",
+                            f"arn:aws:iam::{self.account}:role/EMR_DefaultRole",
+                        ],
+                    ),
+                ]
+            )
